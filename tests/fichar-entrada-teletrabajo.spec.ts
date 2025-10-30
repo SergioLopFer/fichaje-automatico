55e0f5eefe413d62bc7e6a280c78377c62c3c098
SergioLopFer <80686674+SergioLopFer@users.noreply.github.com>
2025-10-30 15:50:48 +0100

    Add files via upload

---
+++ tests/fichar-entrada-teletrabajo.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+import { config } from './config';
+
+test('test', async ({ page }) => {
+  await page.goto(config.baseURL);
+  await page.getByRole('textbox', { name: 'Nombre del usuario' }).click();
+  await page.getByRole('textbox', { name: 'Nombre del usuario' }).fill('25690116J');
+  await page.getByRole('textbox', { name: 'Contraseña' }).click();
+  await page.getByRole('textbox', { name: 'Contraseña' }).fill('CuentaMentor2025!');
+  await page.getByRole('button', { name: 'Iniciar sesión' }).click();
+  await page.locator('#ddlImputaciones').selectOption('TELETRABAJO');
+  await page.getByRole('link', { name: ' Entrada' }).click();
+  await page.getByRole('button', { name: 'OK' }).click();
+});
---
+++ tests/login-people.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+import { config } from './config';
+
+test('test', async ({ page }) => {
+  await page.goto(config.baseURL);
+  await page.getByRole('textbox', { name: 'Nombre del usuario' }).click();
+  await page.getByRole('textbox', { name: 'Nombre del usuario' }).fill('25690116J');
+  await page.getByRole('textbox', { name: 'Contraseña' }).click();
+  await page.getByRole('textbox', { name: 'Contraseña' }).fill('CuentaMentor2025!');
+  await page.getByRole('button', { name: 'Iniciar sesión' }).click();
+});
